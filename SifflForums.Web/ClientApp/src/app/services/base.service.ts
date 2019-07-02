53e968c88085625bbac70c0ee6394b4ffd2f10c3
Arshad Ahmad <arsh90@outlook.com>
2019-07-02 21:23:01 +0800

    Moved api root url to environment variables

--- SifflForums.Web/ClientApp/src/app/services/base.service.ts
+++ SifflForums.Web/ClientApp/src/app/services/base.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
-  protected apiRoot = 'http://localhost:60993/';
+  protected apiRoot = environment.apiRootUrl;
 
   protected httpHeaders: HttpHeaders;
 
