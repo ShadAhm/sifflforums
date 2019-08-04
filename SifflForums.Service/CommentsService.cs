6ebd7e831b5fde5e9dd1faeca6ee884f984b2b3d
Arshad Ahmad <arsh90@outlook.com>
2019-08-04 15:24:49 +0800

    Renamed a variable

--- SifflForums.Service/CommentsService.cs
+++ SifflForums.Service/CommentsService.cs
@@ -45,11 +45,11 @@ namespace SifflForums.Service
 
             if (!string.IsNullOrWhiteSpace(currentUsername))
             {
-                foreach (var item in vms)
+                foreach (var viewModel in vms)
                 {
-                    var entity = entities.SingleOrDefault(o => o.CommentId == item.CommentId);
+                    var entity = entities.SingleOrDefault(o => o.CommentId == viewModel.CommentId);
 
-                    item.CurrentUserVoteWeight = entity.VotingBox.Upvotes.SingleOrDefault(o => o.User.Username == currentUsername)?.Weight ?? 0;
+                    viewModel.CurrentUserVoteWeight = entity.VotingBox.Upvotes.SingleOrDefault(o => o.User.Username == currentUsername)?.Weight ?? 0;
                 }
             }
 
