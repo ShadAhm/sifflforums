610de4316d6a4b5a54190cb10b5f67927be40383
Shad <arsh90@gmail.com>
2019-08-30 15:25:15 +0800

    If there's only one section, navigate to that section

--- SifflForums.Web/ClientApp/src/app/components/home/home.component.ts
+++ SifflForums.Web/ClientApp/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ForumsectionsService } from '../../services/forumsections.service';
 import { ForumSection } from '../../models/forums';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,7 @@ import { ForumSection } from '../../models/forums';
 export class HomeComponent implements OnInit {
   forumSections: ForumSection[]; 
 
-  constructor(private forumsectionsService: ForumsectionsService) { }
+  constructor(private router: Router, private forumsectionsService: ForumsectionsService) { }
 
   ngOnInit() {
     this.getAllSections(); 
@@ -20,6 +21,10 @@ export class HomeComponent implements OnInit {
     this.forumsectionsService.getAll().subscribe(
       (response: ForumSection[]) => {
         this.forumSections = response;
+
+        if (this.forumSections.length == 1) {
+          this.router.navigateByUrl(`/submissions/${this.forumSections[0].forumSectionId}`);
+        }
       },
       (error) => { console.error("Error happened", error) }
     );
