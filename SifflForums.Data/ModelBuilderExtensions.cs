a10b8693924d10aba3e2cf2a8388d32438d421a0
Shad <arsh90@gmail.com>
2019-08-30 14:12:49 +0800

    Moved modelbuilder shit into extension method

---
+++ SifflForums.Data/ModelBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SifflForums.Data.Entities;
+
+namespace SifflForums.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void SetConstraints(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comment>().HasIndex(a => a.VotingBoxId).IsUnique();
+            modelBuilder.Entity<Submission>().HasIndex(a => a.VotingBoxId).IsUnique();
+            modelBuilder.Entity<Upvote>().HasAlternateKey(a => new { a.VotingBoxId, a.UserId });
+        }
+
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
